d6a2753f1433a26c78fd78b0ce198c16a90ea9c8
M. Henry Linder <mhlinder@gmail.com>
2014-09-28 22:27:07 -0400

    now in black-and-white

--- poisson_shape.R
+++ poisson_shape.R
@@ -1,20 +1,18 @@
-pdf("poisson.pdf")
-
+pdf("poisson_bw.pdf")
 
 plot.new()
 plot.window(xlim=c(0, 20), ylim=c(0, 0.5))
 
+pchs <- c(21, 10, 19)
 lambdas <- c(1, 4, 10)
-cols <- c("#8C2318", "#5E8C6A", "#F2C45A")
 for (i in 1:3) {
     ppoisson <- cbind(0:20, dpois(0:20, lambdas[i]))
-    points(ppoisson, pch=21, bg=cols[i])
+    points(ppoisson, pch=pchs[i])
     lines(ppoisson)
 }
 
-title(main="Poisson Distribution")
 axis(1)
 axis(2)
-legend("topright", "lambda", lambdas, pch=21, pt.bg=cols, title=expression(lambda))
+legend("topright", "lambda", lambdas, pch=pchs, title=expression(lambda))
 
 dev.off()
